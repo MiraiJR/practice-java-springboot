344d02320f86d77263467fee2709b2c695d179d0
Trương Văn Hào <truongvanhao159@gmail.com>
2024-07-11 23:27:21 +0700

    implement: Kafka configuration and modify docker compose and application.properties file

--- src/main/java/com/miraijr/examing/modules/user/adapter/out/ConsumerAdapter.java
+++ src/main/java/com/miraijr/examing/modules/user/adapter/out/ConsumerAdapter.java
@@ -3,18 +3,18 @@ package com.miraijr.examing.modules.user.adapter.out;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
-@KafkaListener(topics = { ConsumerAdapter.CREATE_USER_FROM_ACCOUNT })
 public class ConsumerAdapter {
     private final static String CREATE_USER_FROM_ACCOUNT = "create-user-from-account";
-    private final ObjectMapper objectMapper;
+    // private final ObjectMapper objectMapper;
 
+    @KafkaListener(topics = { ConsumerAdapter.CREATE_USER_FROM_ACCOUNT })
     public void createUserFromAccount(Object data) {
-
+        System.out.println("Received: " + data);
     }
 }
