7be435b1038f40811a215164ab537429f12afebe
hao-truong <hao-truong@cybozu.vn>
2024-08-22 17:05:18 +0700

    TASK-10: refactor code

--- src/main/java/com/miraijr/examing/modules/product/adaper/out/persistence/elasticsearch/ProductElasticsearchQuery.java
+++ src/main/java/com/miraijr/examing/modules/product/adaper/out/persistence/elasticsearch/ProductElasticsearchQuery.java
@@ -1,10 +1,13 @@
 package com.miraijr.examing.modules.product.adaper.out.persistence.elasticsearch;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Component;
 import com.miraijr.examing.modules.product.application.port.out.LoadRecommendProductNamePort;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import lombok.AllArgsConstructor;
 
@@ -13,23 +16,26 @@ import lombok.AllArgsConstructor;
 public class ProductElasticsearchQuery implements LoadRecommendProductNamePort {
     private final ElasticsearchClient elasticsearchClient;
     private static final String INDEX = "products";
+    private static final List<String> SEARCH_FIELDS = Arrays.asList("name", "description", "categoryName");
 
     @Override
     public List<String> loadRecommendProductName(String prompt) {
         List<ProductEntityElasticsearch> recommendedProducts = new ArrayList<>();
+        SearchResponse<ProductEntityElasticsearch> search;
         try {
-            SearchResponse<ProductEntityElasticsearch> search = this.elasticsearchClient.search(s -> s
+            search = this.elasticsearchClient.search(s -> s
                     .index(INDEX)
                     .query(q -> q
                             .multiMatch(t -> t
                                     .query(prompt)
-                                    .fields(List.of("name", "description", "categoryName"))
+                                    .fields(SEARCH_FIELDS)
                                     .fuzziness("AUTO"))),
                     ProductEntityElasticsearch.class);
-
             search.hits().hits().forEach(hit -> recommendedProducts.add(hit.source()));
-        } catch (Exception e) {
-            System.out.println("Elasticsearch query failed: " + e.getMessage());
+        } catch (ElasticsearchException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return recommendedProducts.stream().map((product) -> product.getName()).toList();
