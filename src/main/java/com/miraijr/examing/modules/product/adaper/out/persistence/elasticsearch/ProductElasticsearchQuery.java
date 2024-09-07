57ca869da7146728b3bfe63b6ea8cf0a0b9a31ae
hao-truong <hao-truong@cybozu.vn>
2024-09-07 14:37:18 +0700

    TASK-12: Refactor code

--- src/main/java/com/miraijr/examing/modules/product/adaper/out/persistence/elasticsearch/ProductElasticsearchQuery.java
+++ src/main/java/com/miraijr/examing/modules/product/adaper/out/persistence/elasticsearch/ProductElasticsearchQuery.java
@@ -11,66 +11,68 @@ import com.miraijr.examing.modules.product.domain.Product;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.json.JsonData;
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class ProductElasticsearchQuery implements LoadRecommendProductNamePort {
-    private final ElasticsearchClient elasticsearchClient;
-    private final ProductMapping productMapping;
-    private static final String INDEX = "products";
-    private static final List<String> SEARCH_FIELDS = Arrays.asList("name", "description", "categoryName");
+  private final ElasticsearchClient elasticsearchClient;
+  private final ProductMapping productMapping;
+  private static final String INDEX = "products";
+  private static final List<String> SEARCH_FIELDS = Arrays.asList("name", "description");
 
-    @Override
-    public List<Product> loadRecommendProductName(String prompt) throws Exception {
-        List<ProductEntityElasticsearch> recommendedProducts = new ArrayList<>();
-        SearchResponse<ProductEntityElasticsearch> search;
-        try {
-            search = this.elasticsearchClient.search(s -> s
-                    .index(INDEX)
-                    .query(q -> q
-                            .multiMatch(t -> t
-                                    .query(prompt)
-                                    .fields(SEARCH_FIELDS)
-                                    .fuzziness("AUTO"))),
-                    ProductEntityElasticsearch.class);
-            search.hits().hits().forEach(hit -> recommendedProducts.add(hit.source()));
-        } catch (ElasticsearchException | IOException e) {
-            throw new Exception(e.getMessage(), e.getCause());
-        }
-
-        return recommendedProducts.stream().map(this.productMapping::convertFromElasticsearchEntityToDomainEntity)
-                .toList();
+  @Override
+  public List<Product> loadRecommendProductName(String prompt) throws Exception {
+    List<ProductEntityElasticsearch> recommendedProducts = new ArrayList<>();
+    SearchResponse<ProductEntityElasticsearch> search;
+    try {
+      search = this.elasticsearchClient.search(s -> s
+          .index(INDEX)
+          .query(q -> q
+              .multiMatch(t -> t
+                  .query(prompt)
+                  .fields(SEARCH_FIELDS)
+                  .fuzziness("AUTO"))),
+          ProductEntityElasticsearch.class);
+      search.hits().hits().forEach(hit -> recommendedProducts.add(hit.source()));
+    } catch (ElasticsearchException | IOException e) {
+      throw new Exception(e.getMessage(), e.getCause());
     }
 
-    @Override
-    public List<Product> loadRecommendProductName(String prompt, Float minPrice, Float maxPrice) throws Exception {
-        List<ProductEntityElasticsearch> recommendedProducts = new ArrayList<>();
-        SearchResponse<ProductEntityElasticsearch> search;
-        try {
-            search = this.elasticsearchClient.search(s -> s
-                    .index(INDEX)
-                    .query(q -> q
-                            .bool(b -> b
-                                    .must(m -> m
-                                            .multiMatch(t -> t
-                                                    .query(prompt)
-                                                    .fields(SEARCH_FIELDS)
-                                                    .fuzziness("AUTO")))
-                                    .filter(f -> f
-                                            .range(r -> r
-                                                    .number(n -> n.field("price").gte(minPrice.doubleValue())
-                                                            .lte(maxPrice.doubleValue())))))),
-                    ProductEntityElasticsearch.class);
+    return recommendedProducts.stream()
+        .map(this.productMapping::convertFromElasticsearchEntityToDomainEntity)
+        .toList();
+  }
 
-            search.hits().hits().forEach(hit -> recommendedProducts.add(hit.source()));
-        } catch (ElasticsearchException | IOException e) {
-            throw new Exception(e.getMessage(), e.getCause());
-        }
+  @Override
+  public List<Product> loadRecommendProductName(String prompt, Float minPrice, Float maxPrice) throws Exception {
+    List<ProductEntityElasticsearch> recommendedProducts = new ArrayList<>();
+    SearchResponse<ProductEntityElasticsearch> search;
+    try {
+      search = this.elasticsearchClient.search(s -> s
+          .index(INDEX)
+          .query(q -> q
+              .bool(b -> b
+                  .must(m -> m
+                      .multiMatch(t -> t
+                          .query(prompt)
+                          .fields(SEARCH_FIELDS)
+                          .fuzziness("AUTO")))
+                  .filter(f -> f
+                      .range(r -> r
+                          .number(n -> n.field(
+                              "price")
+                              .gte(minPrice.doubleValue())
+                              .lte(maxPrice.doubleValue())))))),
+          ProductEntityElasticsearch.class);
 
-        return recommendedProducts.stream()
-                .map(this.productMapping::convertFromElasticsearchEntityToDomainEntity)
-                .toList();
+      search.hits().hits().forEach(hit -> recommendedProducts.add(hit.source()));
+    } catch (ElasticsearchException | IOException e) {
+      throw new Exception(e.getMessage(), e.getCause());
     }
+
+    return recommendedProducts.stream()
+        .map(this.productMapping::convertFromElasticsearchEntityToDomainEntity)
+        .toList();
+  }
 }
